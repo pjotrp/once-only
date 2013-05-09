5049d05a0e85f634d48e8848085652e2b5851ab1
Pjotr Prins <pjotr.public01@thebird.nl>
2013-05-09 16:06:00 +0200

    Error handler

--- lib/once-only/check.rb
+++ lib/once-only/check.rb
@@ -3,6 +3,14 @@ require "digest"
 module OnceOnly
 
   module Check
+    def Check::get_file_list list
+      list.map { |name|
+        if File.exist?(name)
+          name
+        else
+          nil
+      }.compact
+    end
 
     # Calculate the checksums for each file in the list
     def Check::calc_checksums list
